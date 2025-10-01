e88841334e25eca79b28b67b80d2ef9e72707fdf
caluete <abnercaluete96@gmail.com>
2025-10-01 12:46:46 -0300

    Adicionando a resposta do ex10.py

---
+++ String-dia_26-09/ex10.py
@@ -0,0 +1,6 @@
+#  Escreva um algoritmo em Python que peça ao usuário uma palavra e exiba cada letra da palavra em uma linha separada.
+
+palavra = input("Digite uma palavra:\n")
+
+for i in palavra:
+    print(i)
