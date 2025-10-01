fab3b0e17314a6053e8a6c45588773402a0fdee9
caluete <abnercaluete96@gmail.com>
2025-10-01 11:26:35 -0300

    Adiciona ex09.py

--- String-dia_26-09/ex09.py
+++ String-dia_26-09/ex09.py
@@ -1,2 +1,9 @@
 #  Peça ao usuário uma string e exiba uma nova versão onde cada caractere aparece duplicado. Exemplo: "python" → "ppyytthhoonn".
 
+palavra = input("Digite uma palavra:\n")
+nova_palavra = ""
+
+for i in palavra:
+    nova_palavra += i * 2
+
+print(nova_palavra)
